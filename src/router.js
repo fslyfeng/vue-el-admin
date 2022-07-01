d0b65df0dff7a64d76a583c99900b5cbf9eed6db
netren <fslyfeng@gmail.com>
2022-07-01 21:51:33 +0800

    vue-router二次封装（一）

--- src/router.js
+++ src/router.js
@@ -1,27 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
-import layout from "./views/layout.vue";
+import routes from "./common/config/router.js";
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: "/",
-      name: "layout",
-      component: layout,
-      children: [
-        {
-          path: "/index",
-          name: "index",
-          component: () => import("./views/index/index.vue"),
-        },
-      ],
-    },
-    {
-      path: "/login",
-      name: "login",
-      component: () => import("./views/login/index.vue"),
-    },
-  ],
+  routes,
 });
