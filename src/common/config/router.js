621c0998f54b3cb8f5b4c99a7f8e5a908920fefd
netren <fslyfeng@gmail.com>
2022-07-03 22:12:16 +0800

    vue-router二次封装（四）

--- src/common/config/router.js
+++ src/common/config/router.js
@@ -1,3 +1,10 @@
+/**
+ * 规则：
+ * 一、例如：index/index，shop/index以index结尾的，path和name默认去除index
+ * 二、例如：shop/list，默认生成name为shop_list（如果结尾为index，例如shop/index则是shop）
+ * 三、填写后不会自动生成
+ * **/
+
 let routes = [
   {
     path: "/",
@@ -7,16 +14,20 @@ let routes = [
     component: "layout",
     children: [
       {
-        path: "/index",
-        name: "index",
+        // path: "/index",
+        // name: "index",
         // component: () => import("../../views/index/index.vue"),
         component: "index/index",
+      },
+      {
+        // path: "/shop/goods/list",
+        component: "shop/goods/list",
       },
     ],
   },
   {
-    path: "/login",
-    name: "login",
+    // path: "/login",
+    // name: "login",
     // component: () => import("../../views/login/index.vue"),
     component: "login/index",
   },
@@ -36,6 +47,12 @@ let getRoutes = function () {
 function createRoute(arr) {
   for (let i = 0; i < arr.length; i++) {
     if (!arr[i].component) return;
+    //去除index
+    let val = getValue(arr[i].component);
+    //生成name
+    arr[i].name = arr[i].name || val.replace(/\//g, "_");
+    //生成path
+    arr[i].path = arr[i].path || `/${val}`;
     //自动生成component
     let componentFun = import(`../../views/${arr[i].component}.vue`);
     arr[i].component = () => componentFun;
@@ -44,5 +61,16 @@ function createRoute(arr) {
     }
   }
 }
-
+//支除index
+function getValue(str) {
+  //获取最后一个索引
+  let index = str.lastIndexOf("/");
+  //获取最后一个/后面的值
+  let val = str.substring(index + 1, str.length);
+  //判断是不是index结尾
+  if (val === "index") {
+    return str.substring(index, -1);
+  }
+  return str;
+}
 export default getRoutes();
