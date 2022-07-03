7454c180da3821a2ef835243896440591698cc03
netren <fslyfeng@gmail.com>
2022-07-03 21:10:52 +0800

    vue-router二次封装（二）

--- src/common/config/router.js
+++ src/common/config/router.js
@@ -3,24 +3,46 @@ let routes = [
     path: "/",
     name: "layout",
     redirect: { name: "index" },
-    component: () => import("../../views/layout.vue"),
+    // component: () => import("../../views/layout.vue"),
+    component: "layout",
     children: [
       {
         path: "/index",
         name: "index",
-        component: () => import("../../views/index/index.vue"),
+        // component: () => import("../../views/index/index.vue"),
+        component: "index/index",
       },
     ],
   },
   {
     path: "/login",
     name: "login",
-    component: () => import("../../views/login/index.vue"),
+    // component: () => import("../../views/login/index.vue"),
+    component: "login/index",
   },
   {
     path: "*",
     redirect: { name: "index" },
   },
 ];
+//获取路由信息方法
+let getRoutes = function () {
+  // 生成路由详细信息
+  createRoute(routes);
+  return routes;
+};
 
-export default routes;
+// 自动生成路由
+function createRoute(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (!arr[i].component) return;
+    //自动生成component
+    let componentFun = import(`../../views/${arr[i].component}.vue`);
+    arr[i].component = () => componentFun;
+    if (arr[i].children && arr[i].children.length > 0) {
+      createRoute(arr[i].children);
+    }
+  }
+}
+
+export default getRoutes();
